48e30fdac6d946759cc27367cbe55ea46830ec0e
Christopher Malinao <christopher.malinao@tuitt.com>
2025-06-28 20:53:01 +0800

    removed cors options

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const app = express();
 
 app.use(express.json());
 
-const corsOptions = {
-    origin: ['http://localhost:5173'],
-    credentials: true,
-    optionsSuccessStatus: 200 
-};
+// const corsOptions = {
+//     origin: ['http://localhost:5173'],
+//     credentials: true,
+//     optionsSuccessStatus: 200 
+// };
 
 app.use(cors());
 
